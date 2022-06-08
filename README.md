854ee29c19cfbbec836529f61964b656468f5d05
Charles J. Cliffe <cj@cubicproductions.com>
2022-06-08 01:50:11 -0400

    dependency unpack fixes; update readme

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Requirements:
  - [Visual Studio Community 2022](https://visualstudio.microsoft.com/vs/community/) 
  - [7-zip](https://www.7-zip.org/)
  - [Python](https://www.python.org/downloads/release/python-3105/)
+ - [SDRPlay RSP API](https://www.sdrplay.com/software/SDRplay_RSP_API-Windows-3.09.exe)
 
 Notes:
  - Install Python with option 'Add to PATH' or add manually
--- scripts/build_cubic.bat
+++ scripts/build_cubic.bat
@@ -6,7 +6,8 @@ if not exist wxWidgets-3.1.6 (
     echo Downloading wxWidgets..
     powershell -Command "Invoke-WebRequest https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.6/wxWidgets-3.1.6.zip -OutFile wxWidgets-3.1.6.zip"
     @REM powershell Expand-Archive wxWidgets-3.1.6.zip -DestinationPath wxWidgets-3.1.6
-    7z x wxWidgets-3.1.6.zip
+    mkdir wxWidgets-3.1.6
+    7z x wxWidgets-3.1.6.zip -owxWidgets-3.1.6
     cd wxWidgets-3.1.6/build/msw
     msbuild wx_vc17.sln /property:Configuration=Release /property:Platform=x64
 )

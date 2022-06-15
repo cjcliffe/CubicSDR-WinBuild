5ab0d5c8992216d303b577f4422969c2cfee9729
Charles J. Cliffe <cj@cubicproductions.com>
2022-06-14 23:10:30 -0400

    Download latest MSVC redist; seems to build complete installer but SDR dialog doesn't work

--- scripts/build_cubic.bat
+++ scripts/build_cubic.bat
@@ -14,6 +14,15 @@ if not exist wxWidgets-3.1.6 (
 
 set WXWIDGETS_ROOT_DIR=%CS_DEPS%/wxWidgets-3.1.6
 
+cd %CS_DEPS%
+if not exist vc_redist.x64.exe (
+    echo Downloading MSVC X64 redistributable..
+    powershell -Command "Invoke-WebRequest https://aka.ms/vs/17/release/vc_redist.x64.exe -OutFile vc_redist.x64.exe"
+)
+
+SET BUNDLE_MSVC_REDIST=%CS_DEPS%/vc_redist.x64.exe
+
+
 cd %CS_SOURCES%
 if not exist CubicSDR (
     git clone https://github.com/cjcliffe/CubicSDR 
@@ -28,6 +37,7 @@ cmake -B CubicSDR -G %CS_GENERATOR% -A x64 %BUILD_ROOT%\sources\CubicSDR^
     -DCMAKE_INSTALL_PREFIX:PATH=%CS_INSTALL%/CubicSDR^
     -DSoapySDR_DIR:PATH=%SOAPY_SDR_ROOT%/cmake^
     -DSOAPY_SDR_ROOT:PATH=%SOAPY_SDR_ROOT%^
+    -DBUNDLE_MSVC_REDIST:PAtH=%BUNDLE_MSVC_REDIST%^
     -DBUILD_INSTALLER=ON -DBUNDLE_SOAPY_MODS=ON
 cmake --build CubicSDR --config Release --target install
 
